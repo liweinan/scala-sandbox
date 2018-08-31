9bfef31f5bdc9f0775d07e64bd035d5208f1dc4e
Weinan Li <l.weinan@gmail.com>
2018-08-31 09:32:58 +0800

    .

---
+++ src/main/scala/PrivateClass.scala
@@ -0,0 +1,10 @@
+object PrivateClass extends App {
+  //  package myPackage {
+  //    private[myPackage] class Test
+  //    private object A extends Test
+  //  }
+  //
+  //  package otherPackage {
+  //    object B extends myPackage.Test
+  //  }
+}
