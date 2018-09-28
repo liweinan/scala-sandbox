189780bccf767f1c3764b35c6a816b0e3a9ef663
Weinan Li <l.weinan@gmail.com>
2018-09-28 13:05:09 +0800

    .

--- src/main/scala/deadlock.scala
+++ src/main/scala/deadlock.scala
@@ -25,11 +25,11 @@ class A {
   lazy val x: Int = {
     val t = new Thread() {
       override def run() {
-        self.synchronized {} // 死锁
+        self.synchronized {} // 死锁，主线程和t都在等待对方退出。
       }
     }
     t.start()
-    t.join()
+    t.join() // 主线程等待t结束，但是t在等待主线程结束。
     1
   }
 }
