019131cb0f727f841126f4424fc634b83477d886
Weinan <weli@redhat.com>
2018-10-09 15:41:21 +0800

    .

--- src/main/scala/io/weli/PlayPlay.scala
+++ src/main/scala/io/weli/PlayPlay.scala
@@ -2,9 +2,11 @@ import scala.language.implicitConversions
 
 object PlayPlay extends App {
 
+  // 给Int类型加个chat方法。
   class LoquaciousInt(x: Int) {
     def chat: Unit = for (i <- 1 to x) println("Hi!")
   }
+
 
   implicit def intToLoquaciousInt(x: Int) = new LoquaciousInt(x)
 
