adf6beb3894b7512c00c4659ce886ddbce1657ad
Weinan Li <l.weinan@gmail.com>
2018-10-21 20:36:33 +0800

    .

--- src/main/scala/CountChars.scala
+++ src/main/scala/CountChars.scala
@@ -3,6 +3,5 @@ import scala.io.Source
 if (args.length > 0) {
   for (line <- Source.fromFile(args(0)).getLines())
     println(line.length + " " + line)
-}
-else
+} else
   Console.err.println("Please enter filename")
