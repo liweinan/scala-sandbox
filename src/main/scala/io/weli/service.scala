a74fcf998106e231c5cb6b2d747f43580d32f7f6
Weinan Li <l.weinan@gmail.com>
2018-11-21 20:04:12 +0800

    .

---
+++ src/main/scala/io/weli/service.scala
@@ -0,0 +1,15 @@
+class Service {
+
+  class Logger {
+    def log(message: String): Unit = println(s"log: $message")
+  }
+
+  var logger: Logger = new Logger
+}
+
+val s1 = new Service
+val s2 = new Service
+println(s1.logger.getClass)
+println(s2.logger.getClass)
+println(s1.logger.getClass == s2.logger.getClass)
+s1.logger = s2.logger
