847b15404ead901b4e3be12e0a893be24c2ff491
Weinan Li <l.weinan@gmail.com>
2018-11-21 20:07:13 +0800

    .

--- src/main/scala/io/weli/service.scala
+++ src/main/scala/io/weli/service.scala
@@ -1,15 +1,18 @@
-class Service {
+object ServiceBox {
 
-  class Logger {
-    def log(message: String): Unit = println(s"log: $message")
-  }
-
-  var logger: Logger = new Logger
+  //  class Service {
+  //
+  //    class Logger {
+  //      def log(message: String): Unit = println(s"log: $message")
+  //    }
+  //
+  //    var logger: Logger = new Logger
+  //  }
+  //
+  //  val s1 = new Service
+  //  val s2 = new Service
+  //  println(s1.logger.getClass)
+  //  println(s2.logger.getClass)
+  //  println(s1.logger.getClass == s2.logger.getClass)
+  //  s1.logger = s2.logger
 }
-
-val s1 = new Service
-val s2 = new Service
-println(s1.logger.getClass)
-println(s2.logger.getClass)
-println(s1.logger.getClass == s2.logger.getClass)
-s1.logger = s2.logger
